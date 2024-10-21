e281dc084296be701ba181c69283b54bf9e3ab04
sunyj <sunyijiang@gmail.com>
2024-10-21 09:58:33 +0800

    Release 0.0.5; Check ~/.config/vme.bots for bot keys.

--- vme.py
+++ vme.py
@@ -5,6 +5,8 @@ __all__ = [
     'send_file',
 ]
 
+import os
+import re
 import sys
 import json
 import time
@@ -17,6 +19,7 @@ from urllib.request import Request, urlopen
 URL = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=%s'
 JPG_MAGIC = b'\xFF\xD8\xFF'
 PNG_MAGIC = b'\x89\x50\x4E\x47'
+VME_BOTS_FILE = '~/.config/vme.bots'
 
 
 def send(key, msg):
@@ -84,10 +87,11 @@ def robust_request(req, timeout=5, retries=3, delay=1):
 
 def die_usage():
     print(
-        "Usage:\n"
-        "        vme.py bot-key any_file\n"
-        "        vme.py bot-key < text_or_image_file\n"
-        "        echo your message | vme.py bot-key"
+        f"Usage:\n"
+        f"        vme.py key|name any_file\n"
+        f"        vme.py key|name < text_or_image_file\n"
+        f"        echo your message | vme.py key|name\n\n"
+        f"        Define bot keys in {VME_BOTS_FILE} with 'name key' lines"
     )
     exit(1)
 
@@ -96,7 +100,7 @@ def main():
     if len(sys.argv) < 2:
         die_usage()
 
-    key = sys.argv[1]
+    key = get_bot_key(sys.argv[1])
 
     if sys.stdin.isatty():
         if len(sys.argv) != 3:
@@ -105,6 +109,23 @@ def main():
     else:
         data = sys.stdin.buffer.read()
         send(key, data)
+
+
+def get_bot_key(spec):
+    if is_bot_key(spec):
+        return spec
+
+    with open(os.path.expanduser(VME_BOTS_FILE), 'r') as f:
+        for line in f:
+            vals = line.split()
+            if len(vals) == 2 and vals[0] == spec and is_bot_key(vals[1]):
+                return vals[1]
+
+    raise RuntimeError(f'bot {spec} not found in {VME_BOTS_FILE}')
+
+
+def is_bot_key(spec):
+    return bool(re.match(r'^\w{8}(-\w{4}){3}-\w{12}$', spec))
 
 
 if __name__ == '__main__':
